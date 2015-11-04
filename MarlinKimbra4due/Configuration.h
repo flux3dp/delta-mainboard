32a92d412b2c4f065e65f6deff793212ab83ff13
jimyu15 <jim@flux3dp.com>
2015-11-04 21:38:51 +0800

    Modify Diagonal Rod to 189.75

--- MarlinKimbra4due/Configuration.h
+++ MarlinKimbra4due/Configuration.h
@@ -168,11 +168,11 @@
 //     #define DUMMY_THERMISTOR_998_VALUE 25
 //     #define DUMMY_THERMISTOR_999_VALUE 100
 
-#define TEMP_SENSOR_0 1
-#define TEMP_SENSOR_1 1
-#define TEMP_SENSOR_2 1
+#define TEMP_SENSOR_0 0
+#define TEMP_SENSOR_1 0
+#define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
-#define TEMP_SENSOR_BED 1
+#define TEMP_SENSOR_BED 0
 
 // This makes temp sensor 1 a redundant sensor for sensor 0. If the temperatures difference between these sensors is to high the print will be aborted.
 //#define TEMP_SENSOR_1_AS_REDUNDANT
