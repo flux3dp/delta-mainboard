385f1439e2af86b57cfc2ff734c29b52e4f45fa5
Shuo <shuoen.wu@gmail.com>
2015-12-29 16:59:53 +0800

    speed up pi walkingup led breath

--- MarlinKimbra4due/Marlin_main.cpp
+++ MarlinKimbra4due/Marlin_main.cpp
@@ -852,7 +852,7 @@ inline void update_led_flags(char operation_flag, char wifi_flag) {
     led_st.mode[1] = LED_OFF;
     break;
   case PI_WAKINGUP:
-    led_st.param_a[0] = 0.0003;
+    led_st.param_a[0] = 0.0008;
     led_st.mode[0] = LED_WAVE;
     led_st.mode[1] = LED_OFF;
     break;
